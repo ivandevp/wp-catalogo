b8342810f3acb5a378eb7af2d2c1c1ab90afe203
astephannie <astephannie@gmail.com>
2016-12-31 11:12:59 -0500

    Catalog css

--- wp-content/themes/catalogo/page-catalogo.php
+++ wp-content/themes/catalogo/page-catalogo.php
@@ -26,74 +26,78 @@ get_header(); ?>
 		</div>
 	</section>
 	<section id="fs-container" class="fs-container"></section>
-	<section class="catalog active">
-		<div class="buttons">
-			<button type="button" class="prev">Prev</button>
-			<button type="button" class="next">Next</button>
-			<select class="slide-to">
-				<option data-slide-to="0">Pag 1</option>
-				<option data-slide-to="1">Pag 2-3</option>
-				<option data-slide-to="2">Pag 4-5</option>
-				<option data-slide-to="3">Pag 6-7</option>
-				<option data-slide-to="4">Pag 8-9</option>
-				<option data-slide-to="5">Pag 10-11</option>
-				<option data-slide-to="6">Pag 12-13</option>
-				<option data-slide-to="7">Pag 14-15</option>
-				<option data-slide-to="8">Pag 16-17</option>
-				<option data-slide-to="9">Pag 18-19</option>
-				<option data-slide-to="10">Pag 20-21</option>
-				<option data-slide-to="11">Pag 22-23</option>
-				<option data-slide-to="12">Pag 24-25</option>
-				<option data-slide-to="13">Pag 26-27</option>
-			</select>
-			<button type="button" class="fullscreen">Fullscreen</button>
+	<section class="marcas diagonal-pos">		
+		<h2 class="text-center diagonal-neg">CATÁLOGO VIRTUAL</h2>
+		<div class="triang-abajo diagonal-neg"></div>
+		<div class="catalog active diagonal-neg">
+			<div class="buttons">
+				<button type="button" class="prev"><span class="glyphicon glyphicon-step-backward" aria-hidden="true"></span></button>
+				<button type="button" class="next"><span class="glyphicon glyphicon-step-forward" aria-hidden="true"></span></button>
+				<select class="slide-to">
+					<option data-slide-to="0">Pag 1</option>
+					<option data-slide-to="1">Pag 2-3</option>
+					<option data-slide-to="2">Pag 4-5</option>
+					<option data-slide-to="3">Pag 6-7</option>
+					<option data-slide-to="4">Pag 8-9</option>
+					<option data-slide-to="5">Pag 10-11</option>
+					<option data-slide-to="6">Pag 12-13</option>
+					<option data-slide-to="7">Pag 14-15</option>
+					<option data-slide-to="8">Pag 16-17</option>
+					<option data-slide-to="9">Pag 18-19</option>
+					<option data-slide-to="10">Pag 20-21</option>
+					<option data-slide-to="11">Pag 22-23</option>
+					<option data-slide-to="12">Pag 24-25</option>
+					<option data-slide-to="13">Pag 26-27</option>
+				</select>
+				<button type="button" class="fullscreen"><span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span></button>
+			</div>
+			<ol class="content">
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_001.jpg" class="current">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_001.jpg" alt=" Image 1" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_002.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_002.jpg" alt=" Image 2" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_003.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_003.jpg" alt=" Image 3" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_004.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_004.jpg" alt=" Image 4" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_005.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_005.jpg" alt=" Image 5" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_006.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_006.jpg" alt=" Image 6" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_007.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_007.jpg" alt=" Image 7" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_008.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_008.jpg" alt=" Image 8" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_009.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_009.jpg" alt=" Image 9" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_010.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_010.jpg" alt=" Image 10" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_011.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_011.jpg" alt=" Image 11" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_012.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_012.jpg" alt=" Image 12" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_013.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_013.jpg" alt=" Image 13" />
+				</li>
+				<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_014.jpg">
+					<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_014.jpg" alt=" Image 14" />
+				</li>
+			</ol>
 		</div>
-		<ol class="content">
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_001.jpg" class="current">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_001.jpg" alt=" Image 1" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_002.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_002.jpg" alt=" Image 2" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_003.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_003.jpg" alt=" Image 3" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_004.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_004.jpg" alt=" Image 4" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_005.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_005.jpg" alt=" Image 5" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_006.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_006.jpg" alt=" Image 6" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_007.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_007.jpg" alt=" Image 7" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_008.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_008.jpg" alt=" Image 8" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_009.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_009.jpg" alt=" Image 9" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_010.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_010.jpg" alt=" Image 10" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_011.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_011.jpg" alt=" Image 11" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_012.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_012.jpg" alt=" Image 12" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_013.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_013.jpg" alt=" Image 13" />
-			</li>
-			<li data-image="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_014.jpg">
-				<img class="lazy" data-original="http://catalogothn.com.pe/images/catalogos/catalogo6/CATALOGO%20DEPORTIVOS_Page_014.jpg" alt=" Image 14" />
-			</li>
-		</ol>
-	</section>
-	<p>*Para hacer <strong>zoom</strong>, pasar el puntero del mouse sobre la imagen.</p>
+		<p class="diagonal-neg mt-10">*Para hacer <strong>zoom</strong>, pasar el puntero del mouse sobre la imagen.</p>
+	</section>		
 <?php
 
 get_footer('catalogo'); ?>
