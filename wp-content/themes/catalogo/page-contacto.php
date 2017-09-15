c5fb93770a2c835110a6634b10389a51df25db45
Annie Azaña <astephannie@gmail.com>
2017-09-15 11:54:17 +0000

    updates contact info

--- wp-content/themes/catalogo/page-contacto.php
+++ wp-content/themes/catalogo/page-contacto.php
@@ -12,14 +12,13 @@ get_header(); ?>
 	        <div class="row center detalle-contacto">
                 <div class="col-md-5 col-xs-12">
                 	<p class="txt-blue" style="font-size: 15pt;"><strong>Horario de atención</strong></p>
-					<p style="margin-bottom: 7px; font-size: 14pt;">- Lunes a Viernes de <strong>9:00 AM a 7:00 PM</strong></p>
-					<p style="margin-bottom: 7px; font-size: 14pt;">- Sábados de <strong>9:00 AM a 5:00 PM</strong></p>
+					<p style="margin-bottom: 7px; font-size: 14pt;">- Lunes a Viernes de <strong>8:00 AM a 6:00 PM</strong></p>
+					<p style="margin-bottom: 7px; font-size: 14pt;">- Sábados de <strong>8:00 AM a 4:00 PM</strong></p>
 					<p class="txt-blue" style="font-size: 15pt;"><strong>Correo electrónico</strong></p>
 					<p style="margin-bottom: 7px; font-size: 14pt;">Lima: catalogo@triathlon-sport.com</p>
 					<p style="margin-bottom: 7px; font-size: 14pt;">Provincias: vprovinvias@triathlon-sport.com</p>
 					<p class="txt-blue" style="font-size: 15pt;"><strong>Teléfono CENTRAL</strong></p>
 					<p class="txt-blue" style="font-size: 20pt;"><strong>418 - 3838</strong></p>
-					<p>ANEXOS: Provincias (12 - 13 -15), Lima (18 - 16 - 21)</p>
                 </div>
 	        </div>
 	        <h2 class="text-center" style="font-size: 40px;">VISÍTANOS EN</h2>
@@ -38,8 +37,8 @@ get_header(); ?>
 	        	</div>
 	        	<div class="col-md-6 col-xs-12">
 	        		<div class="map">
-		        		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.9488900185047!2d-77.03444395014202!3d-12.047037645142431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8b60deb566d%3A0x86f8f0975cc2e501!2sTriathlon+Sport!5e0!3m2!1ses!2ses!4v1482083780554" width="100%" height="300" frameborder="0" allowfullscreen></iframe>
-		        		<p>SEDE LIMA</p>
+		        		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.737622086409!2d-77.03466968534829!3d-12.06156519145749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8bfecc45f3d%3A0x4375129ca326f2f9!2sAv.+Jos%C3%A9+G%C3%A1lvez+255%2C+La+Victoria+15033!5e0!3m2!1ses-419!2spe!4v1505475937141" width="100%" height="300" frameborder="0" allowfullscreen></iframe>
+		        		<p>SEDE GÁLVEZ</p>
 	        		</div>
 	        	</div>
 		        <div class="col-md-6 col-xs-12">
